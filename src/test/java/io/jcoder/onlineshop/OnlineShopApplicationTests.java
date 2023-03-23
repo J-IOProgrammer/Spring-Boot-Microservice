8a685bbd3d120d331511149e41fac18655d708fb
J-IOProgrammer <fg.bonny@outlook.com>
2023-03-23 16:00:00 +0430

    write mock project and complete test

--- src/test/java/io/jcoder/onlineshop/OnlineShopApplicationTests.java
+++ src/test/java/io/jcoder/onlineshop/OnlineShopApplicationTests.java
@@ -1,15 +1,23 @@
 package io.jcoder.onlineshop;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jcoder.onlineshop.dto.ProductRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Testcontainers
@@ -20,6 +28,8 @@ class OnlineShopApplicationTests {
     static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry properties) {
@@ -27,7 +37,21 @@ class OnlineShopApplicationTests {
     }
 
     @Test
-    void shouldCreateProduct() {
+    void shouldCreateProduct() throws Exception {
+        ProductRequest productRequest = getProductRequest();
+        String valueAsString = objectMapper.writeValueAsString(productRequest);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(valueAsString))
+                .andExpect(status().isCreated());
+    }
+
+    private ProductRequest getProductRequest() {
+        return ProductRequest.builder()
+                .name("test product")
+                .description("this is a test request")
+                .price(BigDecimal.valueOf(12000))
+                .build();
     }
 
 }
