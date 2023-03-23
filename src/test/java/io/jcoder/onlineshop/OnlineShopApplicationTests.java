63bf0d3539339ae3dab32e1b3b366c686c09436f
J-IOProgrammer <fg.bonny@outlook.com>
2023-03-23 15:11:30 +0430

    add mongoDBContainer and set uri for test

--- src/test/java/io/jcoder/onlineshop/OnlineShopApplicationTests.java
+++ src/test/java/io/jcoder/onlineshop/OnlineShopApplicationTests.java
@@ -2,9 +2,21 @@ package io.jcoder.onlineshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class OnlineShopApplicationTests {
+
+    @Container
+    static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+    static void setProperties(DynamicPropertyRegistry properties) {
+        properties.add("spring.data.mongodb.uri" , mongoDBContainer::getReplicaSetUrl);
+    }
 
     @Test
     void contextLoads() {
